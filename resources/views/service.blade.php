c35df17e0238082a29da770a23be0ffb9b28a523
Mutsanna <am.qoid@gmail.com>
2020-07-17 23:22:50 +0700

    santuyy napa ce

---
+++ resources/views/service.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app')
+
+@section('content')
+	
+
+	<div class="container">
+	
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Special title treatment</h5>
+						<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Special title treatment</h5>
+						<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+			<div class="alert alert-danger" role="alert">
+			A simple danger alert—check it out!
+			</div>
+		<br>
+	<!-- Ini buka contener -->
+		<div class="col-12">
+		<!-- ini adalah row pertrama -->
+			<div class="card border-success mb-3" style="max-width: 18rem;">
+			<!-- Ini content 1 -->
+				<div class="card-header">
+				</div>
+				<div class="card-body text-success">
+					<h5 class="card-title">Designer</h5>
+					<p class="card-text">
+						Seorang desainer grafis memiliki tanggung jawab mewujudkan komunikasi verbal menjadi komunikasi visual agar semua pesan dapat dengan mudah diterima oleh konsumen.</p>
+				</div>
+			</div>
+			<!-- Ini tutup conten 1 -->
+			<!-- ini content 2 -->
+			<div class="card border-danger mb-3" style="max-width: 18rem;">
+				<div class="card-header">
+				</div>
+				<div class="card-body text-danger">
+					<h5 class="card-title">Photographer</h5>
+					<p class="card-text">Fotografer memiliki tugas untuk mengambil foto guna mengabadikan berbagai momen dalam suatu gambar. Contoh konten foto yang diambil oleh fotografer seperti pernikahan, fashion, makanan, arsitektur, dan bentang alam.</p>
+				</div>
+			</div>
+			<!-- Ini tutup conten 2 -->
+			<!-- ini conten 3 -->
+			<div class="card border-warning mb-3" style="max-width: 18rem;">
+				<div class="card-header">
+				</div>
+				<div class="card-body text-warning">
+					<h5 class="card-title">Social Media Management</h5>
+					<p class="card-text">Social Media Admin atau yang di Indonesia akrab dengan sebutan 'admin' atau 'mimin' adalah seseorang yang berada di balik akun media sosial yang bertugas merancang konten tulisan, mempostingnya, dan merespon setiap komentar yang masuk melalui akun media sosial yang ia tangani.</p>
+				</div>
+			</div>
+			<!-- Ini tutup conten 3 -->
+			<!-- ini conten 4 -->
+			<div class="card border-info mb-3" style="max-width: 18rem;">
+				<div class="card-header"></div>
+				<div class="card-body text-info">
+					<h5 class="card-title">Business Analyst</h5>
+					<p class="card-text">Tujuan dari seorang analis bisnis adalah mengidentifikasi masalah dan menemukan solusi agar manajemen dapat beroperasi dengan lebih efisien dan efektif, serta dapat mengembangkan bisnisnya. </p>
+				</div>
+			</div>
+			<!-- Ini tutup conten 4 -->
+			<!-- ini conten 5 -->
+			<div class="card border-light mb-3" style="max-width: 18rem;">
+				<div class="card-header"></div>
+				<div class="card-body">
+					<h5 class="card-title">Marketing Specialist</h5>
+					<p class="card-text">Marketing Specialist memiliki tanggung jawab untuk merencanakan, mengarahkan, atau mengkoordinasikan kebijakan dan program pemasaran, antara lain melihat permintaan untuk produk dan jasa yang ditawarkan oleh perusahaan dan pesaingnya serta mengidentifikasi pelanggan potensial. </p>
+				</div>
+			</div>
+			<!-- Ini tutup conten 5 -->
+		</div>
+	</div>
+
+	
+
+
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+             <h1 class="display-4 text-center">PT Menara Saidah Bahari</h1>
+                <p class="lead"></p>
+                <hr class="my-4">
+                <p>PT Menara Saidah Bahari adalah sebuah perusahaan start up yang menyediakan layanan komersial dan bisnis. Pada awalnya PT Menara Saidah Bahari ini hanya bergerak pada bidang bisnis analisis serta bisnis visual. Namun seiring perkembangan teknologi maka terciptalah layanan baru yang menyesuaikan kebutuhan client. Perusahaan ini memiliki keunggulan pada tidak memiliki alur yang sulit untuk dipahami oleh orang awam sekalipun.</p>
+                <p class="lead">
+                <a class="btn btn-warning btn-lg" href="https://id.wikipedia.org/wiki/Menara_Saidah" role="button">Untuk lebih lanjut.</a>
+            </p>
+        </div>
+    </div>
+@endsection
